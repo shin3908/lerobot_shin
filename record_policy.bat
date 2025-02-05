51ca0c10306ece97f3b35a0e49b9ea0a63a9f691
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-02-05 11:27:27 +0900

    Evaluate policy

---
+++ record_policy.bat
@@ -0,0 +1,12 @@
+python lerobot/scripts/control_robot.py ^
+  --robot.type=koch ^
+  --control.type=record ^
+  --control.single_task="Evaluate Policy for Grasp a block and put it in the hole." ^
+  --control.fps=30 ^
+  --control.repo_id=shin1107/eval_act_koch_train_block ^
+  --control.warmup_time_s=5 ^
+  --control.episode_time_s=30 ^
+  --control.reset_time_s=20 ^
+  --control.num_episodes=10 ^
+  --control.push_to_hub=true ^
+  --control.policy.path=outputs/train/act_koch_train_block/checkpoints/last/pretrained_model
--- visualize.bat
+++ visualize.bat
@@ -1,3 +1,3 @@
 python lerobot/scripts/visualize_dataset_html.py ^
-  --repo-id shin1107/koch_test_block ^
+  --repo-id shin1107/eval_act_koch_train_block ^
   --load-from-hf-hub 1
