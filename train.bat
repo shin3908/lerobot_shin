26e47fb4b1d9c5e11262ec1989f85da21c3a16f5
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-02-10 18:49:37 +0900

    add bash

--- train.bat
+++ train.bat
@@ -1,7 +1,7 @@
 python lerobot/scripts/train.py ^
-  --dataset.repo_id=shin1107/koch_train_block ^
+  --dataset.repo_id=shin1107/koch_move_block_with_some_shapes ^
   --policy.type=act ^
-  --output_dir=outputs/train/act_koch_train_block2 ^
-  --job_name=act_koch_train_block ^
+  --output_dir=outputs/train/koch_move_block_with_some_shapes ^
+  --job_name=act_koch_move_block_with_some_shapes ^
   --device=cuda ^
   --wandb.enable=true
