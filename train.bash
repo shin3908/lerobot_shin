5693f72a014e75c5e05ba6bb2911d85d58fc493f
shinsaku <harrypotter.o1107@gmail.com>
2025-02-12 17:31:31 +0900

    mini change train.bash

--- train.bash
+++ train.bash
@@ -1,12 +1,12 @@
 python lerobot/scripts/train.py \
   --dataset.repo_id=shin1107/koch_move_block_with_some_shapes \
   --policy.type=act \
-  --output_dir=outputs/train/koch_default_resnet18_not_pretrained \
-  --job_name=act_koch_default_resnet18_not_pretrained \
+  --output_dir=outputs/train/koch_default_resnet18_pretrained \
+  --job_name=act_koch_default_resnet18_pretrained \
   --device=cuda \
   --wandb.enable=true \
   --wandb.project=lerobot_VE \
   --policy.vision_backbone="resnet18" \
-  --policy.pretrained_backbone_weights=None 
+  --policy.pretrained_backbone_weights="ResNet18_Weights.IMAGENET1K_V1" 
 
 ※ そのままbashを実行するとエラーが発生するので、ターミナルで実行すること
