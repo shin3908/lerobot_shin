3e3545ec2685731025728886d7dccf0c5e5e9f61
shinsaku <harrypotter.o1107@gmail.com>
2025-02-12 16:25:59 +0900

    change train.bat for compare

--- train.bat
+++ train.bat
@@ -1,7 +1,9 @@
 python lerobot/scripts/train.py ^
   --dataset.repo_id=shin1107/koch_move_block_with_some_shapes ^
   --policy.type=act ^
-  --output_dir=outputs/train/koch_move_block_with_some_shapes ^
-  --job_name=act_koch_move_block_with_some_shapes ^
+  --output_dir=outputs/train/koch_default_resnet18_not_pretrained ^
+  --job_name=act_koch_default_resnet18_not_pretrained ^
   --device=cuda ^
-  --wandb.enable=true
+  --wandb.enable=true ^
+  --policy.vision_backbone="resnet18" ^
+  --policy.pretrained_backbone_weights=None ^
