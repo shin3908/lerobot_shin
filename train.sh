da4cc1aa5a79732669b72fc42b12c62eb7ee4ee7
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-05-21 16:10:30 +0900

    pio-fast

--- train.sh
+++ train.sh
@@ -12,3 +12,13 @@ python lerobot/scripts/train.py \
 ※ そのままbashを実行するとエラーが発生するので、ターミナルで実行すること
 ※ export CUDA_VISIBLE_DEVICES=1
 ※ resnet18: "ResNet18_Weights.IMAGENET1K_V1", resnet50: "ResNet50_Weights.IMAGENET1K_V2"
+
+※ pi0 ファインチューニング用
+python lerobot/scripts/train.py \
+  --dataset.repo_id=shin1107/koch_base \
+  --policy.type=pi0fast \
+  --output_dir=outputs/train/koch_base_pi0fast \
+  --job_name=act_koch_base_pi0fast \
+  --policy.device=cuda \
+  --wandb.enable=true \
+  --wandb.project=lerobot_base 
