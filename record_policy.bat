002dbe185fb29a77024f1be9fac0182530e7bd75
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-06-23 10:58:37 +0900

    marge

--- record_policy.bat
+++ record_policy.bat
@@ -3,7 +3,7 @@ python lerobot/scripts/control_robot.py ^
   --control.type=record ^
   --control.single_task="put the block into the corresponding hole" ^
   --control.fps=30 ^
-  --control.repo_id=shin1107/eval_koch_base_smolvla_pretrained_100000 ^
+  --control.repo_id=shin1107/eval_koch_base_smolvla_pretrained_100000_test ^
   --control.warmup_time_s=5 ^
   --control.episode_time_s=30 ^
   --control.reset_time_s=20 ^
