fae2f017a8ae3aa3c61e1afc46a84926f8bd8ce6
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-06-26 18:21:33 +0900

    scalar ->scalars for rerun

--- record.bat
+++ record.bat
@@ -3,10 +3,10 @@ python -m lerobot.record ^
     --robot.port=COM3 ^
     --robot.id=follower ^
     --robot.cameras="{\"front\": {\"type\": \"opencv\", \"index_or_path\": 0, \"width\": 640, \"height\": 480, \"fps\": 30}, \"top\": {\"type\": \"opencv\", \"index_or_path\": 1, \"width\": 640, \"height\": 480, \"fps\": 30}}" ^
-    --teleop.type=so101_leader ^
+    --teleop.type=koch_leader ^
     --teleop.port=COM4 ^
     --teleop.id=leader ^
     --display_data=true ^
-    --dataset.repo_id=${HF_USER}/record-test ^
-    --dataset.num_episodes=2 ^
-    --dataset.single_task="Grab the black cube"
+    --dataset.repo_id=shin1107/koch_new ^
+    --dataset.num_episodes=160 ^
+    --dataset.push_to_hub=true
