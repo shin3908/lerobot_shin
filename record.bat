91495967f7a437b9659fa06e4802c72f0db3d364
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-06-27 19:06:49 +0900

    mini change bat

--- record.bat
+++ record.bat
@@ -7,6 +7,6 @@ python -m lerobot.record ^
     --teleop.port=COM4 ^
     --teleop.id=leader ^
     --display_data=true ^
-    --dataset.repo_id=shin1107/koch_new2_test ^
-    --dataset.num_episodes=2 ^
+    --dataset.repo_id=shin1107/koch_new2 ^
+    --dataset.num_episodes=160 ^
     --dataset.push_to_hub=true
