93c781635ec1e501e767d1570d4729eb6511f9bc
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-06-27 11:22:57 +0900

    mini change record_policy.bat

--- record_policy_old.bat
+++ record_policy_old.bat
@@ -1,15 +1,29 @@
 chcp 65001 >nul  
 
 set /p MODEL=Enter MODEL (pi0 / smolvla):
-set /p STEP=Enter STEP *1e4 (02 / 04 / 06 / 08 / 10): 
+set /p STEP=Enter STEP *e4 (02 / 04 / 06 / 08 / 10): 
+set /p SHAPE=Enter SHAPE (circle / octagonal / cross / square):
 
 IF "%STEP%"=="" (
     echo [ERROR] STEP is required.
     exit /b 1
 )
+IF "%SHAPE%"=="" (
+    echo [ERROR] SHAPE is required.
+    exit /b 1
+)
 
-IF "%MODEL%"=="" (
-    echo [ERROR] MODEL is required.
+set TASK=""
+IF "%SHAPE%"=="circle" (
+    set TASK=put the cylinder block into the corresponding hole
+) ELSE IF "%SHAPE%"=="octagonal" (
+    set TASK=put the octagonal pillar block into the corresponding hole
+) ELSE IF "%SHAPE%"=="cross" (
+    set TASK=put the cross columnar block into the corresponding hole
+) ELSE IF "%SHAPE%"=="square" (
+    set TASK=put the square block into the corresponding hole
+) ELSE (
+    echo [ERROR] Invalid shape specified: %SHAPE%
     exit /b 1
 )
 
@@ -20,7 +34,8 @@ python -m lerobot.record ^
   --robot.port=COM3 ^
   --robot.id=follower ^
   --robot.cameras="{\"front\": {\"type\": \"opencv\", \"index_or_path\": 0, \"width\": 640, \"height\": 480, \"fps\": 30}, \"top\": {\"type\": \"opencv\", \"index_or_path\": 1, \"width\": 640, \"height\": 480, \"fps\": 30}}" ^
-  --dataset.repo_id=shin1107/eval_koch_base_%MODEL%_pretrained_%STEP%0000 ^
+  --dataset.single_task="%TASK%" ^
+  --dataset.repo_id=shin1107/eval_koch_base_%MODEL%_pretrained_%STEP%0000_%SHAPE% ^
   --dataset.episode_time_s=30 ^
   --dataset.num_episodes=20 ^
   --teleop.type=koch_leader ^
