102ecae5a37b8db536abd917f59022d33f12fae9
shin3908 <okamura.s.ac@m.titech.ac.jp>
2025-06-30 16:05:24 +0900

    mini change record_policy.py

--- record_policy.bat
+++ record_policy.bat
@@ -4,6 +4,7 @@ set /p MODEL=Enter MODEL (pi0 / smolvla):
 set /p STEP=Enter STEP *1e4 (02 / 04 / 06 / 08 / 10): 
 set /p HOLE=Enter HOLE_positon (1 / 2 / 12 / 13 / ...):
 set /p EPISODES=Enter number of EPISODES (20 / 40 / 60 / 80):
+set /p COUNT=Enter number of COUNT (1 / 2 / 3 / ...):
 
 IF "%STEP%"=="" (
     echo [ERROR] STEP is required.
@@ -25,6 +26,11 @@ IF "%EPISODES%"=="" (
     exit /b 1
 )
 
+IF "%COUNT%"=="" (
+    echo [ERROR] COUNT is required.
+    exit /b 1
+)
+
 set CUDA_VISIBLE_DEVICES=0
 
 python -m lerobot.record ^
@@ -32,7 +38,7 @@ python -m lerobot.record ^
   --robot.port=COM3 ^
   --robot.id=follower ^
   --robot.cameras="{\"front\": {\"type\": \"opencv\", \"index_or_path\": 0, \"width\": 640, \"height\": 480, \"fps\": 30}, \"top\": {\"type\": \"opencv\", \"index_or_path\": 1, \"width\": 640, \"height\": 480, \"fps\": 30}}" ^
-  --dataset.repo_id=shin1107/eval_koch_base_%MODEL%_%STEP%0000_Pos%HOLE% ^
+  --dataset.repo_id=shin1107/eval_%MODEL%_%STEP%0000_Pos%HOLE%_%COUNT% ^
   --dataset.num_episodes=%EPISODES% ^
   --teleop.type=koch_leader ^
   --teleop.port=COM4 ^
