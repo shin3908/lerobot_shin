278d65a01091c32907d345e648b287a0b85fb988
shinsaku <harrypotter.o1107@gmail.com>
2025-07-02 11:01:53 +0900

    add visualize.sh

---
+++ visualize_dataset.sh
@@ -0,0 +1,5 @@
+python lerobot/scripts/visualize_dataset_html.py \
+    --repo-id shin1107/koch_new2 \
+    --load-from-hf-hub 1 \
+    --host 127.0.0.2 \
+    --output-dir outputs/visualize_dataset/koch_new2 
